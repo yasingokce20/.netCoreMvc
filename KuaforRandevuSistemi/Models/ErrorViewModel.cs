5c725bf61c75b435a949a86e86050b3d844e5cbd
Yasin Gökçe <mfyasing@gmail.com>
2024-12-20 20:12:32 +0300

    Proje isketi olusturuldu

---
+++ KuaforRandevuSistemi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KuaforRandevuSistemi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
